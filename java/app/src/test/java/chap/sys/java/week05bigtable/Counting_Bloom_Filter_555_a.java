a53062fee83b60349a4bc80e4ad893357643d2a8
fxrcode <fxrcode@gmail.com>
2020-11-21 00:39:19 -0800

    thanks aaronice.gitbook.io to explain 2 types of bloom filter, and laowantong Chap 9 BigTable lec that bloom filter is for Read optimization

---
+++ java/app/src/test/java/chap/sys/java/week05bigtable/Counting_Bloom_Filter_555_a.java
@@ -0,0 +1,41 @@
+package chap.sys.java.week05bigtable;
+
+import org.junit.Test;
+import java.util.*;
+
+public class Counting_Bloom_Filter_555_a {
+    @Test public void test() {
+
+    }
+    public class CountingBloomFilter {
+        /*
+        * @param k: An integer
+        */public CountingBloomFilter(int k) {
+            // do intialization if necessary
+        }
+    
+        /*
+         * @param word: A string
+         * @return: nothing
+         */
+        public void add(String word) {
+            // write your code here
+        }
+    
+        /*
+         * @param word: A string
+         * @return: nothing
+         */
+        public void remove(String word) {
+            // write your code here
+        }
+    
+        /*
+         * @param word: A string
+         * @return: True if contains word
+         */
+        public boolean contains(String word) {
+            // write your code here
+        }
+    }
+}
---
+++ java/app/src/test/java/chap/sys/java/week05bigtable/Standard_Bloom_Filter_556_a.java
@@ -0,0 +1,88 @@
+package chap.sys.java.week05bigtable;
+
+import org.junit.Test;
+import java.util.*;
+
+public class Standard_Bloom_Filter_556_a {
+    @Test
+    public void test() {
+        StandardBloomFilter sbf = new StandardBloomFilter(10);
+        sbf.add("hello");
+        sbf.contains("hell");
+        sbf.contains("helloa");
+        sbf.contains("hello");
+        sbf.contains("hell");
+        sbf.contains("helloa");
+        sbf.contains("hello");
+    }
+
+    /**
+     * 您的提交打败了 26.40% 的提交!
+     */
+    public class StandardBloomFilter {
+        private final int PRIMES[] = { 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109,
+                113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199 };
+        private List<HashFunc> hashs;
+        private BitSet bits;
+        private int k;  // # of hashes (about 15)
+
+        /*
+         * @param k: An integer
+         */public StandardBloomFilter(int k) {
+            // do intialization if necessary
+            this.k = k;
+            this.hashs = new ArrayList<>(k);
+            for (int i = 0; i < k; i++) {
+                hashs.add(new HashFunc(100000 + i, PRIMES[i]));
+            }
+            this.bits = new BitSet(100000 + k);
+        }
+
+        /*
+         * @param word: A string
+         *
+         * @return: nothing
+         */
+        public void add(String word) {
+            // write your code here
+            for (int i = 0; i < k; i++) {
+                bits.set(hashs.get(i).hash(word));
+            }
+        }
+
+        /*
+         * @param word: A string
+         *
+         * @return: True if contains word
+         */
+        public boolean contains(String word) {
+            // write your code here
+            for (int i = 0; i < k; i++) {
+                if (!bits.get(hashs.get(i).hash(word))) {
+                    System.out.println(false);
+                    return false;
+                }
+            }
+            System.out.println(true);
+            return true;
+        }
+
+        class HashFunc {
+            int cap, base;
+
+            public HashFunc(int c, int b) {
+                this.cap = c;
+                this.base = b;
+            }
+
+            public int hash(String s) {
+                int code = 0;
+                for (int i = 0; i < s.length(); i++) {
+                    code = code * base + (int) s.charAt(i);
+                    code %= cap;
+                }
+                return code;
+            }
+        }
+    }
+}
