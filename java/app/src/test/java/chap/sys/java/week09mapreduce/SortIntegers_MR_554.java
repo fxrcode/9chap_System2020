718854eb6468620798f87f386f517f3feb900b6f
fxrcode <fxrcode@gmail.com>
2020-11-26 16:08:47 -0800

    Begin week09 mapreduce

---
+++ java/app/src/test/java/chap/sys/java/week09mapreduce/SortIntegers_MR_554.java
@@ -0,0 +1,32 @@
+package chap.sys.java.week09mapreduce;
+
+public class SortIntegers_MR_554 {
+    public class SortIntegers {
+
+        public static class Map {
+            public void map(int _, List<Integer> value, OutputCollector<String, List<Integer>> output) {
+                // Write your code here
+                // Output the results into output buffer.
+                // Ps. output.collect(String key, List<Integer> value);
+            }
+        }
+
+        public static class Reduce {
+            public void reduce(String key, List<List<Integer>> values, OutputCollector<String, List<Integer>> output) {
+                // Write your code here
+                // Output the results into output buffer.
+                // Ps. output.collect(String key, List<Integer> value);
+            }
+        }
+    }
+
+    /*****************************************************************
+     * All given class defined and ready to use
+     */
+
+    // Definition of OutputCollector:
+    class OutputCollector<K, V> {
+        public void collect(K key, V value);
+        // Adds a key/value pair to the output buffer
+    }
+}
