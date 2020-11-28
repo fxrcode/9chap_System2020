ccafd45d3b5c6c0354df0f8e06cc5fd6edbb7726
fxrcode <fxrcode@gmail.com>
2020-11-28 00:22:40 -0800

    Learned java regex in week10 Url Parser, and used Guava Resources package to read files in resources folder to test

--- java/app/src/test/java/chap/sys/java/week10crawlergooglesuggestion/UrlParser_523.java
+++ java/app/src/test/java/chap/sys/java/week10crawlergooglesuggestion/UrlParser_523.java
@@ -12,6 +12,7 @@ import org.junit.Test;
 
 // Parse a html page, extract the Urls in it. Use regex to parse html.
 public class UrlParser_523 {
+    // used Guava's Resources to read file into a string
     @Test
     public void test() throws IOException {
         String t1 = "UrlParser1.txt";
@@ -24,7 +25,12 @@ public class UrlParser_523 {
     }
 
     public class HtmlParser {
+        // https://regexr.com/
+        // (?i) mode modifier, it means the remainder of expression is case insensitive
+        // (abc) means capture group, it's for extracing a substring or using a backreference
+        // [^xyz] means: Match any character that is NOT in the sat
         private final String HREF_REGEX = "\\s+(?i)href\\s*=\\s*(\"|')+([^\"'\\s]*)";
+
         /*
          * @param content: content source code
          * @return: a list of links
